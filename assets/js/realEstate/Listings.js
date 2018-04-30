4ef6478e8e9339282cd44cee127d8b63cf2848d4
don <don.tinimbang@gmail.com>
2018-04-29 23:27:51 -0500

    work on listing

--- assets/js/realEstate/Listings.js
+++ assets/js/realEstate/Listings.js
@@ -12,8 +12,30 @@ export default class Header extends Component {
 
   render () {
     return (<section className="Listings">
-        Listings
-        
+        <section className="search-area">
+          <input type="text" name="search" />
+        </section>
+
+        <section className="sortby-area">
+          <div>
+            390 results found
+          </div>
+          <div className="sort-options">
+            <select name="sortby" className="sortby">
+              <option value ="price-asc">Highest Price</option>
+              <option value ="price-dsc">Lowest Price</option> 
+            </select>
+          </div>
+        </section>
+
+        <section className="listings-results">
+
+        </section>
+
+        <section className="pagination">
+
+        </section>
+
         </section>)
   }
 }
